1790a4fb958a2da4c920fab6cdef53559e9207fb
qujihan <qujihan@163.com>
2024-08-17 05:45:02 +0800

    update download script

--- download.py
+++ download.py
@@ -3,10 +3,17 @@
 import os
 import sys
 import json
+import time
+import logging
 import zipfile
 import argparse
 import requests
 
+color_reset = "\033[0m"
+color_red = "\033[1;31m"
+color_green = "\033[1;32m"
+color_yellow = "\033[1;33m"
+color_blue = "\033[1;34m"
 
 try:
     from tqdm import tqdm
@@ -14,7 +21,8 @@ try:
     tqdm_installed = True
 except ImportError:
     tqdm_installed = False
-    print("tqdm not installed, will not show progress bar.")
+    print(f"{color_red}tqdm not installed, will not show progress bar.{color_reset}")
+    time.sleep(2)
 
 
 def parse_config(config_path):
@@ -25,10 +33,12 @@ def parse_config(config_path):
         for font_name, urls in data.items():
             fonts_map[font_name] = urls
     except json.JSONDecodeError:
-        print(f"Error: Failed to parse JSON file {config_path}. ")
+        print(
+            f"{color_red} Error: Failed to parse JSON file {config_path}.{color_reset}"
+        )
         sys.exit(1)
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"{color_red}Error: {e}{color_reset}")
         sys.exit(1)
     return fonts_map
 
@@ -45,7 +55,9 @@ def load_config(config_name):
     elif current_config_exist:
         return parse_config(current_config_path)
     else:
-        print(f"Error: {config_name} not found in current and parent dir. ")
+        print(
+            f"{color_red} Error: {config_name} not found in current and parent dir.{color_reset}"
+        )
         sys.exit(1)
 
 
@@ -69,6 +81,7 @@ def download_font(url, path):
 
 
 def extract_font(src, det):
+    print(f"Extracting {color_blue} {os.path.basename(src)} {color_reset}")
     with zipfile.ZipFile(src, "r") as zip_ref:
         zip_ref.extractall(det)
 
@@ -80,19 +93,26 @@ def get_fonts_from_config(fonts_map):
         os.makedirs(curr_font_dir, exist_ok=True)
         for url in urls:
             url = f"{download_proxy}{url}"
-            download_path = os.path.join(curr_font_dir, os.path.basename(url))
-            print(f"Download{font_name} from {url}")
+            basename = os.path.basename(url)
+            download_path = os.path.join(curr_font_dir, basename)
+            print("\n")
+            print(f"Download {color_blue} {basename} {color_reset}")
+            print(f"From {color_green} {url} {color_reset}")
+            print(f"Save to {color_yellow} {download_path} {color_reset}")
             file_type = os.path.splitext(url)[1]
             if file_type == ".zip":
                 download_font(url, download_path)
                 extract_font(download_path, curr_font_dir)
+                print(f"Remove {color_green} {download_path} {color_reset}")
                 os.remove(download_path)
             elif file_type == ".ttf":
                 download_font(url, download_path)
             else:
-                print(f"Unsupported file type: {file_type}. ")
+                print(f"{color_red} Unsupported file type: {file_type}. {color_reset}")
+
 
 download_proxy = ""
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -100,7 +120,9 @@ def main():
     if parser.parse_args().proxy:
         global download_proxy
         download_proxy = "https://mirror.ghproxy.com/"
-
+    print(
+        f"{color_red}If you encounter any network issues, try using the --proxy parameter.{color_reset}\n"
+    )
     fonts_map = load_config("fonts.json")
     get_fonts_from_config(fonts_map)
 
